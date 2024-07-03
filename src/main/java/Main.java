b4f4aee1c2388a7744e46d9e2aa30b7b20025522
Tomáš Klouček <tktomasek@gmail.com>
2024-07-03 23:12:14 +0200

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,27 @@
 // Uncomment this block to pass the first stage
 // import java.util.Scanner;
 
+import Commands.ICommand;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+
+        // Initialize implemented commands
+        Map<String, ICommand> commands = new HashMap<>();
         // Uncomment this block to pass the first stage
         System.out.print("$ ");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+
+        if (commands.containsKey(input.toLowerCase())) {
+            commands.get(input.toLowerCase()).exec(null);
+        } else {
+            System.out.println(input+": command not found");
+        }
     }
 }
